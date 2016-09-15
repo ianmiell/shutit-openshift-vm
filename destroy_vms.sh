ecdf0c5143e46d8b7072ea3ad23b17ceb296bbb3
Ian Miell <ian.miell@gmail.com>
2016-09-15 15:10:55 +0100

    destroy

--- destroy_vms.sh
+++ destroy_vms.sh
@@ -2,13 +2,13 @@
 while true 
 do
 	echo DESTROYING
-	VBoxManage list runningvms | grep '^"origin' | awk '{print $1}' | xargs -IXXX VBoxManage controlvm 'XXX' poweroff && VBoxManage list vms | awk '{print $1}'  | xargs -IXXX VBoxManage unregistervm 'XXX' --delete
-	if [[ $(VBoxManage list vms | wc -l) == '0' ]]
+	VBoxManage list runningvms | grep '^"origin' | awk '{print $1}' | xargs -IXXX VBoxManage controlvm 'XXX' poweroff && VBoxManage list vms | grep '^"origin' | awk '{print $1}'  | xargs -IXXX VBoxManage unregistervm 'XXX' --delete
+	if [[ $(VBoxManage list vms | grep '^"origin' | wc -l) == '0' ]]
 	then
 		break
 	else
 		ps -ef | grep virt
-		ps -ef | grep virtualbox | grep 'origin' | awk '{print $2}' | xargs kill
+		ps -ef | grep virtualbox | grep '^"origin' | awk '{print $2}' | xargs kill
 	fi
 	echo DESTROYING
 done
